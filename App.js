fb8cfd1de086db3f2e0d53cae2691e7c9ebe49fb
Rey Joshua <reyjoshuamacarat@gmail.com>
2020-11-14 01:11:49 +0800

    added stack navigation

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import MapViewScreen from './screens/MapViewScreen';
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {/* Add your screens here */}
+        <Stack.Screen name='MapViewScreen' component={MapViewScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
