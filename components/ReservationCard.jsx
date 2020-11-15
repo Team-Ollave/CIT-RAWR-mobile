635f7196c132a56cdf96807985ba65071ca2c3ca
Rey Joshua <reyjoshuamacarat@gmail.com>
2020-11-15 17:29:37 +0800

    unified component stylesheet, added Featured Rooms Card

--- components/ReservationCard.jsx
+++ components/ReservationCard.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import EStyleSheet from 'react-native-extended-stylesheet';
-import { Typography } from '../utls/typography';
-import { Colors } from '../utls/colors';
+import { View, Text } from 'react-native';
+import { estyles } from './styles';
 
 export default function ReservationCard({
   eventName,
@@ -22,24 +20,3 @@ export default function ReservationCard({
     </View>
   );
 }
-
-const estyles = EStyleSheet.create({
-  container: {
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    padding: '0.75rem',
-    borderRadius: '0.5rem',
-    borderColor: Colors.gray3,
-    borderWidth: 1,
-    marginBottom: '1rem',
-  },
-  eventName: {
-    ...Typography.regular('Poppins_500Medium'),
-    color: Colors.black,
-  },
-  requestorName: {
-    ...Typography.extraSmall('Poppins_400Regular'),
-    color: Colors.gray4,
-  },
-});
