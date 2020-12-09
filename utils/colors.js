3c558e1a9c3eba5ed5c978972f5fc63996e5fdc5
Rey Joshua <reyjoshuamacarat@gmail.com>
2020-12-10 03:36:25 +0800

    added badge background colors

--- utils/colors.js
+++ utils/colors.js
@@ -7,4 +7,7 @@ export const Colors = {
   gray4: '#717171',
   accentColor: '#741E1E',
   citYellow: '#FACA10',
+  acceptedGreen: '#27ae60',
+  pendingYellow: '#faca10',
+  declinedRed: '#eb5757',
 };
